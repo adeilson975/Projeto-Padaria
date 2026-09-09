66eebdbd717370fac8be8220a7c1619887752168
adeilsonassis <adeilsonassis@gmail.com>
2026-09-09 17:28:25 -0300

    add mais uma função no menu hamburguer

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const botaoMenu = document.querySelector(".menu-hamburguer");
 const menu = document.querySelector("header nav");
+const iconeMenu = botaoMenu.querySelector("i");
+
 botaoMenu.addEventListener("click", function() {
     menu.classList.toggle("menu-aberto");
+
+    iconeMenu.classList.toggle("fa-xmark");
+    iconeMenu.classList.toggle("fa-bars");
     
 })
